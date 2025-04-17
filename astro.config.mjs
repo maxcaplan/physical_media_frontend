17a775a68885083743181c901625023749294be8
maxcaplan <maxacaplan@gmail.com>
2025-04-17 13:41:26 -0300

    Initialized tailwind and preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
+import { fileURLToPath, URL } from "node:url"
+
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }
+    }
+  },
+
+  prefetch: {
+    prefetchAll: true,
+  },
+
+  adapter: netlify(),
+  integrations: [preact({ devtools: true })]
+});
